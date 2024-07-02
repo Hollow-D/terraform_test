34944bff3440864dc073ddbd21cdf37267084a6f
Hollow-D <68171917+Hollow-D@users.noreply.github.com>
2024-07-02 23:44:07 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -26,28 +26,3 @@ resource "alicloud_security_group" "default" {
   name   = "default"
   vpc_id = alicloud_vpc.vpc.id
 }
-
-resource "alicloud_security_group_rule" "allow_all_tcp" {
-  type              = "ingress"
-  ip_protocol       = "tcp"
-  nic_type          = "intranet"
-  policy            = "accept"
-  port_range        = "1/65535"
-  priority          = 1
-  security_group_id = alicloud_security_group.default.id
-  cidr_ip           = "0.0.0.0/0"
-}
-
-resource "alicloud_instance" "instance" {
-  # cn-hangzhou
-  availability_zone = "cn-hangzhou-b"
-  security_groups   = alicloud_security_group.default.*.id
-
-  # series III
-  instance_type              = "ecs.t5-lc2m1.nano"
-  system_disk_category       = "cloud_efficiency"
-  image_id                   = "ubuntu_20_04_x64_20G_alibase_20240530.vhd"
-  instance_name              = "test_foo"
-  vswitch_id                 = alicloud_vswitch.vsw.id
-  internet_max_bandwidth_out = 10
-}
